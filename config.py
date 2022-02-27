73caa303d782728df6fff4822a405a67aa402fa7
jaqbiak <jaqbiak88@gmail.com>
2022-02-27 16:52:22 +0100

    deelting .env from repo

--- config.py
+++ config.py
@@ -7,16 +7,14 @@ dotenv.load_dotenv()
 
 class Config(object):
     CSRF_ENABLED = True
+    DATABASE_URL = os.environ["DATABASE_URL"]
+    SECRET_KEY = os.environ["DATABASE_SECRET"]
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    DATABASE_URL = os.environ['PRODUCTION_DATABASE_URL']
-    SECRET_KEY = os.environ['PRODUCTION_DATABASE_SECRET']
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    DATABASE_URL = os.environ['DEVELOPMENT_DATABASE_URL']
-    SECRET_KEY = os.environ['DEVELOPMENT_DATABASE_SECRET']
     
